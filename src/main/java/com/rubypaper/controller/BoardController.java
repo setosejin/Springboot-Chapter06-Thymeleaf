524d7d6d38502c3cd4cc3e3fc43dc4e40cb78462
Sejin Kim <lemon05090@naver.com>
2022-06-24 10:07:28 +0900

    Initial commit

---
+++ src/main/java/com/rubypaper/controller/BoardController.java
@@ -0,0 +1,57 @@
+package com.rubypaper.controller;
+
+import com.rubypaper.domain.Board;
+import com.rubypaper.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import java.util.List;
+
+@Controller
+public class BoardController {
+
+	@Autowired
+	private BoardService boardService;
+
+	@RequestMapping("/getBoardList")
+	public String getBoardList(Model model, Board board) {
+		List<Board> boardList = boardService.getBoardList(board);
+		model.addAttribute("boardList", boardList); // jsp에서 함수의 리턴값을 사용하기 위해서는 model에 담아줍니다.
+		return "getBoardList";
+	}
+
+	@GetMapping("/insertBoard")
+	public String insertBoardView() {
+		return "insertBoard";
+	}
+
+	@PostMapping("/insertBoard")
+	public String insertBoard(Board board) {
+		boardService.insertBoard(board);
+		return "redirect:getBoardList";
+	}
+
+	@GetMapping("/getBoard")
+	public String getBoard(Board board, Model model) {
+		model.addAttribute("board", boardService.getBoard(board));
+		return "getBoard";
+	}
+
+	@PostMapping("/updateBoard")
+	public String updateBoard(Board board) {
+		boardService.updateBoard(board);
+		return "forward:getBoardList";
+	}
+
+	@GetMapping("/deleteBoard")
+	public String deleteBoard(Board board) {
+		boardService.deleteBoard(board);
+		return "forward:getBoardList";
+	}
+
+}
