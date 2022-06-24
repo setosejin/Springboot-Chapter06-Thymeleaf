a55500f2571a86d5e566c339d514cf687700adea
Sejin Kim <lemon05090@naver.com>
2022-06-24 14:37:03 +0900

    [Authorization]

--- src/main/java/com/rubypaper/controller/BoardController.java
+++ src/main/java/com/rubypaper/controller/BoardController.java
@@ -1,33 +1,95 @@
 package com.rubypaper.controller;
 
 import com.rubypaper.domain.Board;
+import com.rubypaper.domain.Member;
 import com.rubypaper.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@SessionAttributes("member")
 @Controller
 public class BoardController {
 
 	@Autowired
 	private BoardService boardService;
 
-	@GetMapping("/hello")
-	public String hello(Model model) {
-		model.addAttribute("greeting", "Hello World");
-		return "hello";
+	@ModelAttribute("member")
+	public Member setMember() {
+		return new Member();
+	}
+	//387. 사용자 인증 이후
+	@RequestMapping("/getBoardList")
+	public String getBoardList(@ModelAttribute("member") Member member, Model model, Board board) {
+		if (member.getId() == null) {
+			return "redirect:login";
+		}
+
+		List<Board> boardList = boardService.getBoardList(board);
+
+		model.addAttribute("boardList", boardList);
+		return "getBoardList";
 	}
 
+	@GetMapping("/insertBoard")
+	public String insertBoardView(@ModelAttribute("member") Member member) {
+		if (member.getId() == null) {
+			return "redirect:login";
+		}
+
+		return "insertBoard";
+	}
+
+	@PostMapping("/insertBoard")
+	public String insertBoard(@ModelAttribute("member") Member member, Board board) {
+		if (member.getId() == null) {
+			return "redirect:login";
+		}
+
+		boardService.insertBoard(board);
+		return "redirect:getBoardList";
+	}
+
+	@GetMapping("/getBoard")
+	public String getBoard(@ModelAttribute("member") Member member, Board board, Model model) {
+		if (member.getId() == null) {
+			return "redirect:login";
+		}
+
+		model.addAttribute("board", boardService.getBoard(board));
+		return "getBoard";
+	}
+
+	@PostMapping("/updateBoard")
+	public String updateBoard(@ModelAttribute("member") Member member, Board board) {
+		if (member.getId() == null) {
+			return "redirect:login";
+		}
+
+		boardService.updateBoard(board);
+		return "forward:getBoardList";
+	}
+
+	@GetMapping("/deleteBoard")
+	public String deleteBoard(@ModelAttribute("member") Member member, Board board) {
+		if (member.getId() == null) {
+			return "redirect:login";
+		}
+
+		boardService.deleteBoard(board);
+		return "forward:getBoardList";
+	}
+
+
+	/*
+	//사용자 인증 이전
 	@RequestMapping("/getBoardList")
 	public String getBoardList(Model model, Board board) {
 		List<Board> boardList = boardService.getBoardList(board);
-		model.addAttribute("boardList", boardList); // jsp에서 함수의 리턴값을 사용하기 위해서는 model에 담아줍니다.
+		model.addAttribute("boardList", boardList);
 		return "getBoardList";
 	}
 
@@ -58,6 +120,11 @@ public class BoardController {
 	public String deleteBoard(Board board) {
 		boardService.deleteBoard(board);
 		return "forward:getBoardList";
+	}
+*/
+	@GetMapping("/hello")
+	public void hello(Model model){
+		model.addAttribute("greeting", "Hello 타임리프.^^");
 	}
 
 }
--- src/main/java/com/rubypaper/controller/LoginController.java
+++ src/main/java/com/rubypaper/controller/LoginController.java
@@ -1,7 +1,5 @@
 package com.rubypaper.controller;
 
-import com.rubypaper.domain.Member;
-import com.rubypaper.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+
+import com.rubypaper.domain.Member;
+import com.rubypaper.service.MemberService;
 
 @SessionAttributes("member")
 @Controller
